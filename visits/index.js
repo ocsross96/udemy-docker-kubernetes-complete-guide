29de28e1a82685392673300eed840e4a88edc3d1
Ross Matthews <ross.matthews@ratedpeople.com>
2020-07-24 12:11:48 +0100

    Picking up again

--- visits/index.js
+++ visits/index.js
@@ -10,7 +10,7 @@ const client = redis.createClient({
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
-  process.exit(0);
+  // process.exit(0); uncomment to simulate what happens when there is an error
   client.get('visits', (err, visits) => {
     res.send('Number of visits is ' + visits);
     client.set('visits', parseInt(visits) + 1);
